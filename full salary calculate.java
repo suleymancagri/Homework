2ca82395490c84dc557244ddc740df764766ca3f
suleymancagri <84632602+suleymancagri@users.noreply.github.com>
2021-09-08 01:20:22 +0300

    Add files via upload

---
+++ full salary calculate.java
@@ -0,0 +1,36 @@
+package week1;
+
+import java.util.Scanner;
+
+public class FullSalaryCalculator {
+    public static void main(String[] args){
+
+        System.out.println("Please write the base salary:");
+        Scanner scanner = new Scanner(System.in);
+        int baseSalary = scanner.nextInt();
+        if(baseSalary > 1000)
+        {
+            System.out.println("Please write weekdayworkinghourswithoutovertime:");
+            int weekdayworkinghourswithoutovertime = scanner.nextInt();
+            System.out.println("weekdayovertime :");
+            int weekdayovertime = scanner.nextInt();
+            System.out.println("weekendovertime :");
+            int weekendovertime = scanner.nextInt();
+            scanner.close();
+            double hourlywage= baseSalary / 40;
+            double finalsalary= 0;
+
+            if ( weekdayworkinghourswithoutovertime >=40 && weekendovertime >10)
+            {
+                finalsalary = weekdayworkinghourswithoutovertime*hourlywage + weekdayovertime*hourlywage * 1.5 + weekendovertime*hourlywage*2 + 500;
+            }
+            else{
+                finalsalary = weekdayworkinghourswithoutovertime*hourlywage + weekdayovertime*hourlywage * 1.5 + weekendovertime*hourlywage*2;
+            }
+
+            System.out.println("Your salary is : " + finalsalary);
+        }
+        else
+            System.out.print("Your salary must be bigger than 1000");
+    }
+}
